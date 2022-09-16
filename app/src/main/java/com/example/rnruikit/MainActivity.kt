e63f0e3661a6e08d314856548e50e6c748c2d5c1
Piyachat Jamsai <piyachat@amityeko.com>
2022-09-16 14:59:50 +0700

    add module hello

--- app/src/main/java/com/example/rnruikit/MainActivity.kt
+++ app/src/main/java/com/example/rnruikit/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.rnruikit
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import com.example.testlibrary.HelloMsg
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val textView = findViewById<TextView>(R.id.txtView)
+
+        val msg = HelloMsg().getMsg()
+
+        textView.text = msg
     }
 }
---
+++ testlibrary/src/main/java/com/example/testlibrary/HelloMsg.kt
@@ -0,0 +1,9 @@
+package com.example.testlibrary
+
+class HelloMsg {
+    val message: String = "Hello World"
+
+    fun getMsg(): String {
+        return message
+    }
+}
